1b03d6e58efa729f85d19d590f014bd6a66a3274
Nguyễn Xuân Sơn <165804217+SonNX24042005@users.noreply.github.com>
2025-03-01 21:45:49 +0700

    Update Bai6_5.java

--- Lab1/Bai6_5.java
+++ Lab1/Bai6_5.java
@@ -5,7 +5,6 @@ public class Bai6_5{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Getting array size from user
         System.out.print("Nhập số phần tử: ");
         int size = scanner.nextInt();
         
