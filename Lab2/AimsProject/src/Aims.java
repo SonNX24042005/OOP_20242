f193cbccb84db16ea67af04988755c93341f3833
Nguyễn Xuân Sơn <nguyenxuanson24042005@gmail.com>
2025-03-26 02:57:51 +0700

    update

--- Lab2/AimsProject/src/Aims.java
+++ Lab2/AimsProject/src/Aims.java
@@ -1,9 +1,31 @@
-
 public class Aims {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // Create a new cart
+        Cart anOrder = new Cart();
 
-	}
+        // Create new dvd objects and add them to the cart
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
 
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        // Display cart items and total cost
+        System.out.println("Cart before removing item:");
+        anOrder.displayCart();
+
+        // Remove a DVD from the cart
+        anOrder.removeDigitalVideoDisc(dvd2);
+
+        // Display cart items and total cost after removal
+        System.out.println("\nCart after removing item:");
+        anOrder.displayCart();
+    }
 }
--- Lab2/AimsProject/src/DigitalVideoDisc.java
+++ Lab2/AimsProject/src/DigitalVideoDisc.java
@@ -5,29 +5,36 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
-    // Constructor with category, title, and cost
-    public DigitalVideoDisc(String category, String title, float cost) {
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-    }
+	
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	
+	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.length = length;
+		this.cost = cost;
+	}
 
-    // Constructor with director, category, title, and cost
-    public DigitalVideoDisc(String director, String category, String title, float cost) {
-        this.director = director;
-        this.category = category;
-        this.title = title;
-        this.cost = cost;
-    }
+	public DigitalVideoDisc(String title, String category, String director, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.director = director;
+		this.cost = cost;
+	}
 
-    // Constructor with all attributes
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-    }
+	public DigitalVideoDisc(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+	}
+
 	public String getTitle() {
 		return title;
 	}
